d1057ae303ba854fc8dcb9452392d13f7e2c7276
PinnaclexDesigns <90589994+PinnaclexDesigns@users.noreply.github.com>
2023-03-09 00:51:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,64 @@
-from flask import Flask, render_template, request
 import pandas as pd
-
-app = Flask(__name__)
+import random
 
 # Load the holiday data from the CSV file
 holiday_data = pd.read_csv('holidays.csv')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# Define a function to return a random hotel from the filtered data
+def get_random_hotel(filtered_data):
+    return filtered_data.iloc[random.randint(0, len(filtered_data)-1)]
 
-@app.route('/recommend', methods=['POST'])
-def recommend():
-    # Get the user's preferences from the HTML form
-    category = request.form['category']
-    location = request.form['location']
-    max_price = float(request.form['max_price'])
-
-    # Filter the holiday data based on the user's preferences
+# Define a function to filter the holiday data based on user input
+def filter_data(category, location, max_price):
     filtered_data = holiday_data[(holiday_data['Category'] == category) & 
                                  (holiday_data['Location'] == location) &
                                  (holiday_data['Price Per Night'] <= max_price)]
+    return filtered_data
 
-    # Sort the filtered data by star rating and temperature rating
-    sorted_data = filtered_data.sort_values(by=['Star Rating', 'Temperature Rating'], ascending=False)
+# Define a function to handle user input and return a recommended holiday location
+def get_recommendation(user_input):
+    if 'lazy' in user_input.lower() or 'relaxing' in user_input.lower():
+        category = 'Lazy'
+    else:
+        category = 'Active'
 
-    # Convert the sorted data to a list of dictionaries for use in HTML template
-    hotels = sorted_data.to_dict('records')
+    if 'europe' in user_input.lower():
+        location = 'Europe'
+    elif 'asia' in user_input.lower():
+        location = 'Asia'
+    elif 'north america' in user_input.lower():
+        location = 'North America'
+    elif 'south america' in user_input.lower():
+        location = 'South America'
+    elif 'africa' in user_input.lower():
+        location = 'Africa'
+    elif 'australia' in user_input.lower():
+        location = 'Australia'
+    else:
+        return "Sorry, I'm not sure which continent you're interested in. Please try again."
 
-    return render_template('recommend.html', hotels=hotels)
+    try:
+        max_price = float(user_input.split('$')[1])
+    except:
+        max_price = 100
 
-if __name__ == '__main__':
-    app.run()
+    filtered_data = filter_data(category, location, max_price)
+
+    if len(filtered_data) == 0:
+        return "Sorry, I couldn't find any holiday locations that match your preferences. Please try again with different preferences."
+
+    recommended_hotel = get_random_hotel(filtered_data)
+    return f"I recommend {recommended_hotel['Hotel Name']} in {recommended_hotel['City']}, {recommended_hotel['Country']}. It has a {recommended_hotel['Star Rating']} star rating and a {recommended_hotel['Temperature Rating']} temperature rating. The price per night is {recommended_hotel['Price Per Night']}."
+
+# Start the chatbot
+print("Welcome to the holiday location recommender chatbot! Please enter your questions below. To exit, type 'quit'.")
+
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() == 'quit':
+        print("Chatbot: Goodbye!")
+        break
+
+    response = get_recommendation(user_input)
+    print("Chatbot:", response)
