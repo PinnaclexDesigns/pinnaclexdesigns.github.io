4c8b5c0eb849ad64cd5aa2ccc6f629667f653645
PinnaclexDesigns <90589994+PinnaclexDesigns@users.noreply.github.com>
2023-03-09 01:13:25 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+pip install flask
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    # Retrieve user preferences from form data
+    preference_1 = request.form.get('preference_1')
+    preference_2 = request.form.get('preference_2')
+    budget = request.form.get('budget')
+
+    # Use preferences and budget to recommend holidays
+    recommended_holidays = recommend_holidays(preference_1, preference_2, budget)
+
+    # Pass recommended holidays to template for rendering
+    return render_template('recommendations.html', holidays=recommended_holidays)
+
+def recommend_holidays(preference_1, preference_2, budget):
+    # TODO: Implement logic for recommending holidays based on user preferences and budget
+    pass
+
+if __name__ == '__main__':
+    app.run()
